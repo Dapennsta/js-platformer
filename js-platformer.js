42de7a6835ab1524c9d97b900df3da753a165548
dapennsta <dapennsta@gmail.com>
2016-03-08 05:00:30 +0000

    Added DOMDisplay functionality with css helpers and index

--- js-platformer.js
+++ js-platformer.js
@@ -76,7 +76,12 @@ function Player(pos) {
 }
 Player.prototype.type = "player";
 
-// Lava object
+// Lava object has three different types depending on it's character
+// default behaviour is to reverse direction after collision
+// = will move right along x axis with a speed of 2
+// | will move downwards along y axis with a speed of 2
+// v will move downwards along y axis with a speed of 3
+// and will also repeat position after collision
 function Lava(pos, ch) {
   this.pos = pos;
   this.size = new Vector(1, 1);
@@ -91,6 +96,7 @@ function Lava(pos, ch) {
 }
 Lava.prototype.type = "lava";
 
+// Coins will have a slight vertical wobble
 function Coin(pos) {
   this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
   this.size = new Vector(0.6, 0.6);
@@ -101,3 +107,91 @@ Coin.prototype.type = "coin";
 // Simple test to check proper code
 // var simpleLevel = new Level(simpleLevelPlan);
 // console.log(simpleLevel.width, "by", simpleLevel.height);
+
+// shortcut to create DOM element with class
+function elt(name, className) {
+  var elt = document.createElement(name);
+  if (className) elt.className = className;
+  return elt;
+}
+
+// Used to display level using DOM elements
+function DOMDisplay(parent, level) {
+  this.wrap = parent.appendChild(elt("div", "game"));
+  this.level = level;
+  
+  this.wrap.appendChild(this.drawBackground());
+  this.actorLayer = null;
+  this.drawFrame();
+}
+
+var scale = 20;
+
+// Uses a table to draw the grid of the background
+DOMDisplay.prototype.drawBackground = function() {
+  var table = elt("table", "background");
+  table.style.width = this.level.width * scale + "px";
+  this.level.grid.forEach(function(row) {
+    var rowElt = table.appendChild(elt("tr"));
+    rowElt.style.height = scale + "px";
+    row.forEach(function(type) {
+      rowElt.appendChild(elt("td", type));
+    });
+  });
+  return table;
+};
+
+// Creates a new child for each actor with proper size and position
+DOMDisplay.prototype.drawActors = function() {
+  var wrap = elt("div");
+  this.level.actors.forEach(function(actor) {
+    var rect = wrap.appendChild(elt("div", "actor " + actor.type));
+    rect.style.width = actor.size.x * scale + "px";
+    rect.style.height = actor.size.y * scale + "px";
+    rect.style.left = actor.pos.x * scale + "px";
+    rect.style.top = actor.pos.y * scale + "px";
+  });
+  return wrap;
+};
+
+// Uses actorLayer to redraw actors every frame
+DOMDisplay.prototype.drawFrame = function() {
+  if (this.actorLayer)
+    this.wrap.removeChild(this.actorLayer);
+  this.actorLayer = this.wrap.appendChild(this.drawActors());
+  this.wrap.className = "game" + (this.level.status || "");
+  this.scrollPlayerIntoView();
+};
+
+// Used to scroll view to keep player in center
+DOMDisplay.prototype.scrollPlayerIntoView = function() {
+  var width = this.wrap.clientWidth;
+  var height = this.wrap.clientHeight;
+  var margin = width / 3;
+  
+  // The viewport
+  var left = this.wrap.scrollLeft, right = left + width;
+  var top = this.wrap.scrollTop, bottom = top + height;
+  
+  var player = this.level.player;
+  var center = player.pos.plus(player.size.times(0.5)).times(scale);
+  
+  if (center.x < left + margin)
+    this.wrap.scrollLeft = center.x - margin;
+  else if (center.x > right - margin)
+    this.wrap.scrollLeft = center.x + margin - width;
+  else if (center.y < top + margin)
+    this.wrap.scrollTop = center.y - margin;
+  else if (center.y > bottom - margin)
+    this.wrap.scrollTop = center.y + margin - height;
+};
+
+// Clear screen
+DOMDisplay.prototype.clear = function() {
+  this.wrap.parentNode.removeChild(this.wrap);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  var simpleLevel = new Level(simpleLevelPlan);
+  var display = new DOMDisplay(document.body, simpleLevel);
+});
